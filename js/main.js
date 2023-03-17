c49cdacc104b1e3d5f33da6a4493472c12968976
vaduska <vadimsalapugin@gmail.com>
2023-03-17 15:23:42 +0500

    Added catalog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+const categoriesItem = document.querySelectorAll('.catalog__categories-item-link');
+const cardContent = document.querySelectorAll('.catalog__card-list');
+
+
+function changeActiveForCategory(event) {
+    const categoryTarget = event.currentTarget;
+    const button = categoryTarget.dataset.button;
+
+    categoriesItem.forEach((el) => {
+        el.classList.remove('catalog__categories-item-link-active');
+    });
+
+    categoryTarget.classList.add('catalog__categories-item-link-active');
+
+    cardContent.forEach((el) => {
+        el.classList.remove('catalog__card-list-active');
+    });
+
+    document.querySelector(`#${button}`).classList.add('catalog__card-list-active');
+}
+
+
+categoriesItem.forEach((el) => {
+    el.addEventListener('click', changeActiveForCategory);
+})
+
+
 const swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
